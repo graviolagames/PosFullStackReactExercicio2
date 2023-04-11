c1441bbe9e72064003e97ef8bdc58961ccc82ec7
graviolagames <graviolagames@gmail.com>
2023-04-11 12:37:54 -0300

    Improved MovieDetail

--- react-movies/src/views/MovieDetail.js
+++ react-movies/src/views/MovieDetail.js
@@ -1,38 +1,26 @@
-import {useState } from "react";
+import {useEffect,useState } from "react";
 import { MoviesService } from "../api/MoviesService";
 import { useParams } from "react-router-dom";
+
 export function MovieDetail() {
     const [movie, setMovie] = useState({});
     const { movieId } = useParams();
-    const getMovie = async () => {
-        const { data } = await MoviesService.getMovieDetail(movieId);
-        setMovie(data);
-    };
-    getMovie();
+    
+    useEffect(()=>{
+        MoviesService.getMovieDetail(movieId).then(({data})=>setMovie(data)
+        );
+    },[movieId]);
+    
     return (
         <section className="movie-detail">
-            <div className="container">
-                <div className="row gx-5">
-                    <div className="col-6">
-                    {movie.poster_path && <img
-                    src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`} alt="" />}
-                    </div>
-                <div className="col-6">
-                    <h1>{movie.title}</h1>
-                    <ul>
-                    <li>Budget: {movie.budget}</li>
-                    <li>Original language:
-                    {movie.original_language}</li>
-                    <li>Popularity: {movie.popularity}</li>
-                    </ul>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-12">
-                        {movie.overview}
-                    </div>
-                </div>
-            </div>
+            <h1>{movie.title}</h1>
+            <img
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                width= "150"
+                alt="Movie thumb"
+            />
+            <strong>Original language:</strong>{movie.original_language}
+            <p>{movie.overview}</p>
         </section>
     );
 };
